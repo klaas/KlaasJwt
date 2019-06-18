fb222667e50eed22aef517062d6e0dca5b266ad0
Klaas <klaas@users.noreply.github.com>
2019-06-18 10:59:25 +0200

    guaka sub

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 		.package(url: "https://github.com/klaas/CupertinoJWT", .branch("master")),
 		.package(url: "https://github.com/klaas/ParkBenchCommon", from: "2.0.0"),
 		.package(url: "https://github.com/klaas/Pogging.git", from: "2.0.0"),
-		.package(url: "https://github.com/klaas/Guaka.git", .branch("master_neu")),
+        .package(url: "https://github.com/klaas/Guaka.git", .branch("master-klaas")),
 	],
 	targets: [
 		.target(name: "qjwt", dependencies: ["ParkBench", "Pogging", "Guaka", "SwiftJWT", "CupertinoJWT"]),
